4e851d1e80643a1697d0b229792e8af2f95d3a24
rramires <ricardo@flexbr.com>
2021-10-04 15:10:08 -0300

    Criação do filtro básico

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const api = require('./api')
 const symbol = process.env.SYMBOL
 
 setInterval(async () => {
-    // faz a chamada
-    //console.log('Chamou a API :',  await api.time())
+    /*
+    console.log('Chamou a API :',  await api.time())
     //console.log('ExchangeInfo :',  await api.exchangeInfo())
+    */
+
+    let buy = 0, sell = 0
+
+    // faz a chamada no book
     const result = await api.depth(symbol)
     console.log('Symbol: ',  symbol)
-    console.log('Highest Buy: ',  result.bids[0][0])
-    console.log('Lowest Sell: ',  result.asks[0][0])
+
+    // filtra testando se não está vazio
+    if(result.bids && result.bids.length){
+        console.log('Highest Buy: ',  result.bids[0][0])
+        buy = parseInt(result.bids[0][0])
+    }
+    if(result.asks && result.asks.length){
+        console.log('Lowest Sell: ',  result.asks[0][0])
+        sell = parseInt(result.asks[0][0])
+    }
+
+    // Implemente sua estratégia
+    if(sell < 50000){
+        console.log('Compre !!!')
+    }
+    else if(buy > 60000){
+        console.log('Venda !!!')
+    }
+    else{
+        console.log('Esperando o mercado sair do lugar...')
+    }
 }, process.env.CRAWLER_INTERVAL)
+
